bee025e3f00f5c4fa7d76f4949e432c5dabad7ca
GE Scott Knauss <scott@immauss.com>
2025-02-18 06:22:37 +0000

    trying to move the start.sh out of /opt/tak/

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <version>"
     exit 1
 fi
-tak=$1
+tag=$1
 docker buildx build -t immauss/tak-tak:$tag -f Dockerfile.tak . --load
 docker buildx build -t immauss/tak-db:$tag -f Dockerfile.db . --load
